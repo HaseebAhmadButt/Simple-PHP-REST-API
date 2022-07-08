9914e03f1a3b48513eb7fae3ae53fb16d68d9c77
Hafiz Haseeb Ahmad Butt <67764636+HaseebAhmadButt@users.noreply.github.com>
2022-07-08 13:00:34 +0000

    Update index.php

--- index.php
+++ index.php
@@ -515,14 +515,14 @@ elseif (isset($_GET["country"]) or isset($_GET["team-matches-played"]) or (isset
 else
 {
 	if ($action == "/") {
-        echo "<h2>1. Queries Examples<h2>";
-        echo "<h4>2. To Get Data About Specific Country use Query: /data/{Country Name}</h4>";
-        echo "<h4>3. To Get All Data Uue Query: /data or /data/</h4>";
-        echo "<h4>4. To Get Sorted Data: /data/sor={Field Name}</h4>";
+        echo "<h2>Queries Examples<h2>";
+        echo "<h4>1. To Get Data About Specific Country use Query: /data/{Country Name}</h4>";
+        echo "<h4>2. To Get All Data Uue Query: /data or /data/</h4>";
+        echo "<h4>3. To Get Sorted Data: /data/sor={Field Name}</h4>";
         echo "<h5 style='color: red'>Note, In single Query use only one Field/Attribute to sort data multiple attributes in single query is not supported</h5>";
-        echo "<h4>5. To Get Data according to set of attributes use Query: /data?{Attribute Name}[lte OR gte: this is optional]";
+        echo "<h4>4. To Get Data according to set of attributes use Query: /data?{Attribute Name}[lte OR gte: this is optional]";
         echo "<h5 style='color: red'>OR</h5>";
-        echo "<h4>6. /data?{Attribute Name}[lte OR gte: this is optional]&{Attribute Name}[lte OR gte: this is optional]....</h4>";
+        echo "<h4>5. /data?{Attribute Name}[lte OR gte: this is optional]&{Attribute Name}[lte OR gte: this is optional]....</h4>";
         echo "<h5 style='color: red'>Note: Data According to Country Can't Be Fetched According to this Rule. Use Query 1 To Get Specific Data According to Country</h5>";
 
 
